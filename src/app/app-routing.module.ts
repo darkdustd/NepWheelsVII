25f61dd96f9f33c6a515b2b3e81842e64c8eb101
darkdustd <nbernal783@unab.edu.co>
2019-04-09 20:34:37 -0500

    Nep1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {NerrpComponent} from './nerrp/nerrp.component';
+import {RentComponent} from './rent/rent.component';
+import {AdminComponent} from './admin/admin.component';
+import {ReserveComponent} from './reserve/reserve.component';
+
+const routes: Routes = [
+
+  {path: '', component: HomeComponent},
+  {path: 'Home', component: HomeComponent},
+  {path: 'Login', component: LoginComponent},
+  {path: 'Rent', component: RentComponent},
+  {path: 'Admin', component: AdminComponent},
+  {path: 'Reserve', component: ReserveComponent},
+  {path: '**', component: NerrpComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
